b02b1cfb6109eeab21ebc38ae283b87c4806ed33
Nzovia <nickiemaundu19@gmail.com>
2022-10-23 19:57:29 +0300

    modifying design for reset password page

--- lib/pages/reset_password.dart
+++ lib/pages/reset_password.dart
@@ -2,24 +2,25 @@ import 'package:firebase_auth/firebase_auth.dart';
 import 'package:flutter/material.dart';
 import 'package:kejani/pages/user_login.dart';
 
-class ResetPassword extends StatefulWidget{
+class ResetPassword extends StatefulWidget {
   const ResetPassword({Key? key}) : super(key: key);
 
   @override
   _ResetPasswordState createState() => _ResetPasswordState();
 }
-class _ResetPasswordState extends State<ResetPassword> {
 
+class _ResetPasswordState extends State<ResetPassword> {
   late String _email;
   final auth = FirebaseAuth.instance;
 
   @override
   Widget build(BuildContext context) {
     return Scaffold(
-      appBar: AppBar( backgroundColor: Colors.transparent,
+      appBar: AppBar(
+        backgroundColor: Colors.transparent,
         elevation: 0,
         leading: IconButton(
-          icon: const Icon(Icons.arrow_back, color: Colors.black12),
+          icon: const Icon(Icons.arrow_back, color: Colors.black),
           onPressed: () {
             // passing this to our root
             Navigator.push(
@@ -28,51 +29,75 @@ class _ResetPasswordState extends State<ResetPassword> {
             );
           },
         ),
+        centerTitle: true,
+        title: const Text(
+          "Password Reset",
+          style: TextStyle(color: Colors.black, fontWeight: FontWeight.w500),
+        ),
       ),
-      body: Column(
-        mainAxisAlignment: MainAxisAlignment.center,
-        children: [
-          Padding(
-            padding: const EdgeInsets.all(8.0),
-            child: TextField(
-
-              keyboardType: TextInputType.emailAddress,
-              decoration: const InputDecoration(
-                  hintText: 'Email'
+      body: Padding(
+        padding: const EdgeInsets.all(12.0),
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 80,
+            ),
+            Text(
+              "Create New Password",
+              style: TextStyle(
+                fontSize: 24,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[700],
               ),
+            ),
+            const SizedBox(
+              height: 16,
+            ),
+            Text(
+              "Kindly enter a valid email address associated with your account",
+              style: TextStyle(
+                fontSize: 16,
+                fontWeight: FontWeight.bold,
+                color: Colors.grey[700],
+              ),
+              textAlign: TextAlign.center,
+            ),
+            const SizedBox(
+              height: 50,
+            ),
+            TextField(
+              keyboardType: TextInputType.emailAddress,
+              decoration: const InputDecoration(hintText: 'Email'),
               onChanged: (value) {
                 setState(() {
                   _email = value.trim();
                 });
               },
             ),
-          ),
-          Row(
-              mainAxisAlignment: MainAxisAlignment.spaceAround,
-              children: [
-                    MaterialButton(
-                  minWidth: double.infinity,
-                  height: 50,
-                  onPressed: () {
-                    auth.sendPasswordResetEmail(email: _email);
-                    Navigator.of(context).pop();
-                  },
-                  color: Colors.indigoAccent[400],
-                  shape: RoundedRectangleBorder(
+            const SizedBox(
+              height: 16,
+            ),
+            MaterialButton(
+              minWidth: double.infinity,
+              height: 40,
+              onPressed: () {
+                auth.sendPasswordResetEmail(email: _email);
+                Navigator.of(context).pop();
+              },
+              color: Colors.indigoAccent[400],
+              shape: RoundedRectangleBorder(
                   borderRadius: BorderRadius.circular(40)),
-                  child: const Text(
-                  "Send Request",
-                  style: TextStyle(
-                  fontWeight: FontWeight.w600,
-                  fontSize: 16,
-                  color: Colors.white70),
-                  ),
-                  ),
-              ]
-          )
-
-        ],),
+              child: const Text(
+                "Reset Password",
+                style: TextStyle(
+                    fontWeight: FontWeight.w600,
+                    fontSize: 16,
+                    color: Colors.black),
+              ),
+            ),
+          ],
+        ),
+      ),
     );
   }
-
 }
